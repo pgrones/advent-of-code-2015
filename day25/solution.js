fc44258a95c3cebe8a226208ad9c64ba1eb835ac
pgr3931 <pascal.grones@gmx.at>
2024-07-07 13:12:55 +0200

    day 25 optimizations

--- day25/solution.js
+++ day25/solution.js
@@ -4,26 +4,23 @@ const [goalY, goalX] = [...input.matchAll(/(?<!\+)\d+/g)].map((x) =>
   parseInt(x[0])
 );
 
-let x = 1;
 let y = 2;
 let value = 20151125;
 
 outer: while (true) {
-  let currX = x;
+  let currX = 1;
   let currY = y;
 
   while (currY > 0) {
     value = (value * 252533) % 33554393;
 
-    if (currX === goalX && currY == goalY) {
-      console.log(value);
-      break outer;
-    }
+    if (currX === goalX && currY == goalY) break outer;
 
     currX++;
     currY--;
   }
 
-  x = 1;
   y++;
 }
+
+console.log(value);
